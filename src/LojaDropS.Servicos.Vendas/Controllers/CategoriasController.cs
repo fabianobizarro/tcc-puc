2453ea41cf68af6f7e2911d0c0d5ad3b809641b2
Fabiano Bizarro <fabianoasbizarro@gmail.com>
2019-04-16 21:57:57 -0300

    implementação do CRUD de categorias

--- src/LojaDropS.Servicos.Vendas/Controllers/CategoriasController.cs
+++ src/LojaDropS.Servicos.Vendas/Controllers/CategoriasController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LojaDropS.Dominio;
 using LojaDropS.Infra.Interfaces;
 using LojaDropS.Servicos.Vendas.ViewModels;
 using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,7 +13,7 @@ namespace LojaDropS.Servicos.Vendas.Controllers
     [Route("api/[controller]")]
     [EnableCors("AppPolicy")]
     [ApiController]
-    public class CategoriasController : ControllerBase
+    public class CategoriasController : AppControllerBase
     {
         private readonly ICategoriaStore _store;
 
@@ -24,15 +22,95 @@ namespace LojaDropS.Servicos.Vendas.Controllers
             _store = store;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Get()
         {
             var categorias = await _store.Categorias
-                .Where(p => p.SubCategorias != null)
                 .ToListAsync();
 
             var vm = categorias.Select(DisplayCategoriaViewModel.Map);
 
             return Ok(vm);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var categoria = await _store.Categorias
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            var vm = DisplayCategoriaViewModel.Map(categoria);
+
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CadastroCategoriaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _store.AddAsync(new Categoria(model.Nome));
+                var vm = DisplayCategoriaViewModel.Map(result);
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody]CadastroCategoriaViewModel model)
+        {
+            try
+            {
+                var categoria = await _store.Categorias.FirstOrDefaultAsync(p => p.Id == id);
+                if (categoria == null)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    categoria.Nome = model.Nome;
+                    var result = await _store.UpdateAsync(categoria);
+
+                    var vm = DisplayCategoriaViewModel.Map(result);
+
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                var categoria = await _store.Categorias.FirstOrDefaultAsync(p => p.Id == id);
+                if (categoria == null)
+                {
+                    return NotFound();
+                }
+
+                await _store.DeleteAsync(categoria);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
         }
     }
 }
---
+++ src/LojaDropS.Servicos.Vendas/ViewModels/CadastroCategoriaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaDropS.Servicos.Vendas.ViewModels
+{
+    public class CadastroCategoriaViewModel
+    {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; }
+
+    }
+}
